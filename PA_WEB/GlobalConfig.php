2ee9e7d501cffa71dcb2d643b4fb59653a92a71b
Aduasatu <alan992001@gmail.com>
2022-11-13 21:22:06 +0800

    Hosting

--- PA_WEB/GlobalConfig.php
+++ PA_WEB/GlobalConfig.php
@@ -1,9 +1,9 @@
 <?php
 
-    $_SERVER = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name = "schedulekucing";
+    $_SERVER = "sql202.epizy.com";
+    $username = "epiz_32985961";
+    $password = "0UY62WNTMTIya";
+    $db_name = "epiz_32985961_schedulekucing";
 
     $db = new mysqli($_SERVER, $username, $password, $db_name);
     
